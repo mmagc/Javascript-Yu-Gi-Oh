29aaec33dc99f4821b54ca7b8be3ad6689cae151
mmagc <119635234+mmagc@users.noreply.github.com>
2025-01-20 22:43:49 -0300

    Implementando a lógica de Duelos

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -92,11 +92,41 @@ async function setCardsField(cardId) {
     state.fieldCards.player.src = cardData[cardId].img;
     state.fieldCards.computer.src = cardData[computerCardId].img;
 
-    // let duelResults = await checkDuelResults(cardId, computerCardId);
+    let duelResults = await checkDuelResults(cardId, computerCardId);
 
-    // await updateScore();
-    // await drawButtons(duelResults);
+    await updateScore();
+    await drawButton(duelResults);
 
+}
+
+async function drawButton(text) {
+
+    state.actions.button.innerText = text;
+    state.actions.button.style.display = "block";
+    
+}
+
+async function updateScore() {
+    state.score.scoreBox.innerText = `Win: ${state.score.playerScore} | Lose: ${state.score.computerScore}`;
+}
+
+async function checkDuelResults(playerCardId, computerCardId) {
+    
+    let duelResults = "Empate";
+    let playerCard = cardData[playerCardId];
+
+    if (playerCard.WinOf.includes(computerCardId)) {
+        duelResults = "GANHOU";
+        state.score.playerScore++;
+
+    }
+    
+    if (playerCard.LoseOf.includes(computerCardId)) {
+        duelResults = "Perdeu";
+        state.score.computerScore++;
+    }
+
+    return duelResults;
 }
 
 async function removeAllCardsImages() {
